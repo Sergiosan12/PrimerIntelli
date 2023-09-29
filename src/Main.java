484d8fba05f60b9b4b3ec08114abe4f3b51818a2
Sergio <ssanromangiraldez@danielcastelao.org>
2023-09-29 08:54:45 +0200

    modificacion en el sout

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,6 @@ public class Main {
         // Press Alt+Intro with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
-        System.out.printf("bla bla");
+        System.out.println("modificacion");
     }
 }
